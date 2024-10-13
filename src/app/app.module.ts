8b5ca52e9d5c4b90253480799b7406fe338d9e1c
pawan999333 <pawansharma19feb@gmail.com>
2024-10-13 14:56:52 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TempConverterComponent } from './components/temp-converter/temp-converter.component';
+import { TempConverterPips } from './pipes/TempConverter.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TempConverterComponent,
+    TempConverterPips
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/temp-converter/temp-converter.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-temp-converter',
+  templateUrl: './temp-converter.component.html',
+  styleUrls: ['./temp-converter.component.css']
+})
+export class TempConverterComponent {
+
+valueInF=0;
+valueInC=0;
+
+
+}
